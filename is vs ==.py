2e46dca0b990e099db259b302ce1031aa958d8d5
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-01 18:49:04 -0600

    Add files via upload

---
+++ is vs ==.py
@@ -0,0 +1,40 @@
+print(True == bool(1))
+print('1' == 1)
+print([] == 1)
+print(10 == 10.0)
+print([] == [])
+print([1,2,3] == [1,2,3])
+
+print(True is 1)
+print('1' is 1)
+print([] is 1)
+print(10 is 10.0)
+print([] is [])
+print([1,2,3] is [1,2,3])
+
+print(True is True)
+print('1' is '1')
+print([] is [])
+print(10 is 10.0)
+print([1,2,3] is [1,2,3])
+
+# is check the extact thing
+# == check the value 
+
+print(True is True)
+print('1' is '1')
+print([] is [])
+print(10 is 10.0)
+a = [1,2,3]
+b = [1,2,3]
+print(a == b)
+
+
+
+
+
+
+
+
+
+
